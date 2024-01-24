b5803fd3cdf66f1bfd0947a2b66d5d25bbe0c0e0
homris <mhomris2006@gmail.com>
2024-01-24 17:13:21 +0300

    Updated return.py & added example CSV files
    Added some CSV files for examples and testing. Also added number of Successful, Unsuccessful, Neutral Funds in CSV file

--- return.py
+++ return.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot
 import pandas as pd
 
 #Convert CSV into DataFrame
-df = pd.read_csv("CSV Files/baslangic.csv")
+df = pd.read_csv("CSV Files/01.01.2023-02.01.2024-değişken.csv")
 print(df.to_string())
 
 #Parse float from string and turn into US decimal points(.)
@@ -33,13 +33,25 @@ std_return_string = str(round(std_return, 4)).replace('.', ',')
 print("\nStandart Sapma (Popülasyon): %" + std_return_string)
 
 #Başarılı, Nötr, Başarısız Fonlar
+basarili_count = 0
+basarisiz_count = 0
+nötr_count = 0
+
 for getiri in df['Getiri (%)']:
     if getiri > mean_return + std_return:
         print("Başarılı")
+        basarili_count += 1
     elif getiri > mean_return - std_return:
         print("Nötr")
+        nötr_count += 1
     else:
         print("Başarısız")
+        basarisiz_count += 1
+
+print(str(basarili_count) + " Başarılı")
+print(str(nötr_count) + " Nötr")
+print(str(basarisiz_count) + " Başarısız")
+
 
 
 #Making graphs/charts
